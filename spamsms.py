487eb1a56a9803a8d98642b811c356bc87e253bc
Rizal solehudin <35882424+xonxezdev@users.noreply.github.com>
2018-01-31 19:26:25 +0700

    Add files via upload
    thank for the download this tools and don't use for crime guys :D this is for tester

---
+++ spamsms.py
@@ -0,0 +1,16 @@
+import requests
+import sys,os,time
+import random
+
+no = raw_input('number phone > ')
+
+url='http://www.sendanonymoussms.com/'
+while True:
+	http = requests.post(url, data={'name':no, 'country':'INDONESIA', 'ReceiverNumber':'6285655943464', 'message':'warning this is a spammer by xonxez team :) ', 'Submit':'submit'})
+	content = http.content
+	if 'Your message has been sent! ' in content:
+		print "[+] Message has been sended "
+		time.sleep(10)
+	else:
+		print "[-] Failed to send a message"
+		time.sleep(10)
