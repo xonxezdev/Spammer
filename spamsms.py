49b2f22e8c32c31dfe989f5ea1acea9c47f6a070
Rizal solehudin <35882424+xonxezdev@users.noreply.github.com>
2018-01-31 19:28:23 +0700

    Update spamsms.py

--- spamsms.py
+++ spamsms.py
@@ -5,12 +5,18 @@ import random
 no = raw_input('number phone > ')
 
 url='http://www.sendanonymoussms.com/'
-while True:
-	http = requests.post(url, data={'name':no, 'country':'INDONESIA', 'ReceiverNumber':'6285655943464', 'message':'warning this is a spammer by xonxez team :) ', 'Submit':'submit'})
-	content = http.content
-	if 'Your message has been sent! ' in content:
-		print "[+] Message has been sended "
-		time.sleep(10)
-	else:
-		print "[-] Failed to send a message"
-		time.sleep(10)
+try:
+	while True:
+		http = requests.post(url, data={'name':no, 'country':'INDONESIA', 'ReceiverNumber':'6285655943464', 'message':'warning this is a spammer by xonxez team :) ', 'Submit':'submit'})
+		content = http.content
+		if 'Your message has been sent! ' in content:
+			print "[+] Message has been sended "
+			time.sleep(10)
+		else:
+			print "[-] Failed to send a message"
+			time.sleep(10)
+except IOError:
+	print "exiting from program"
+except KeyboardInterrupt:
+	print "exiting from program"
+	
